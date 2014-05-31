12c67727066f6ea7962a8fad9145c2dceebea6f6
Cameron Paul <cpaul37@gmail.com>
2014-05-30 22:28:47 -0700

    Setup tox

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='DiceBox',
+    version='0.0.0',
+    description='Library for Rolling Dice',
+    author='Cameron Paul',
+    author_email='cpaul37@gmail.com',
+    url='https://github.com/campaul/dicebox',
+    packages=['dicebox'],
+)
