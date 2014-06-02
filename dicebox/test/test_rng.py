04af0b4d1a9be81741da9ee569531a63c88942cc
Cameron Paul <cpaul37@gmail.com>
2014-06-01 22:28:35 -0700

    Added rng logger test

--- dicebox/test/test_rng.py
+++ dicebox/test/test_rng.py
@@ -1,6 +1,6 @@
 import pytest
 
-from dicebox.rng import uniform, xkcd
+from dicebox.rng import logged, uniform, xkcd
 
 
 class TestRng(object):
@@ -13,8 +13,18 @@ class TestRng(object):
     def end(self):
         return 20
 
+    @pytest.fixture
+    def rolls(self):
+        return 5
+
     def test_uniform(self, start, end):
         assert type(uniform(start, end)) == int
 
     def test_xkcd(self, start, end):
         assert type(xkcd(start, end)) == int
+
+    def test_logged(self, start, end, rolls):
+        log = []
+        logged_rng = logged(uniform, log)
+        result = [logged_rng(start, end) for i in range(rolls)]
+        assert result == log
